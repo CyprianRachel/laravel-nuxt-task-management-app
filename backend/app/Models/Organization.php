2af227796a5b4c52c67bc9785c759942635b8b93
CyprianRachel <cyprianrachel@gmail.com>
2025-01-11 14:58:14 +0100

    Updating model Organization.php

---
+++ backend/app/Models/Organization.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Organization extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'phone',
+        'description',
+    ];
+
+    // Relacja odwrotna z User
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
