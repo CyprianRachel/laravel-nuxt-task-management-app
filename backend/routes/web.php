4ca4e65ce5cc1407936104cb373e20620d205f49
CyprianRachel <cyprianrachel@gmail.com>
2025-01-11 20:48:58 +0100

    Web.php back to start settings

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,21 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\OrganizationController;
 
-// Publiczne (bez tokena)
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-// Wymaga tokenu (auth:sanctum)
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/organization/add-member', [OrganizationController::class, 'addMember']);
-
-    // testowe: zwraca aktualnego usera
-    Route::get('/profile', function (Request $request) {
-        return $request->user();
-    });
+Route::get('/', function () {
+    return view('welcome');
 });
-
